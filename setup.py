400ac42280d07e025fbf77ffa9e205829c6ff140
anand <anand.jeyahar@gmail.com>
2016-06-12 00:11:28 +0530

    had to rename the package because i can't get by old account back damn it..

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
-setup(name='dbadmin',
+setup(name='databaseadmin',
       version='1.0',
       description = 'just a python wrapper for doing most DB admin functions',
-      author = 'Anand Jeyahar',
-      author_email = 'anand.jeyahar@gmail.com',
+      author = 'Software Mechanic',
+      author_email = 'softwaremechanic32@gmail.com',
       url = 'https://github.com/emofeedback/python-DbAdmin.git',
       packages= ['dbadmin'],
       )
